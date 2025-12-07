17f1f5d2cb8ea963727870c1f8476bd2a87cf2a7
Paul Brouwer <pbrouwer@offertevergelijker.nl>
2025-12-07 01:23:15 +0100

    package update

--- src/Config/bullwark.php
+++ src/Config/bullwark.php
@@ -2,7 +2,7 @@
 
 return [
     'api_url' => env("BULLWARK_API_URL", "http://core-service-svc.auth.svc.cluster.local/api/v1"),
-    'jwk_url' => env("BULLWARK_JWK_URL", "http://core-service-svc.auth.svc.cluster.local/.well-known/jwks.json"),
+    'jwk_url' => env("BULLWARK_JWK_URL", "http://core-service-svc.auth.svc.cluster.local/.well-known/jwks"),
     "customer_uuid" => env("BULLWARK_CUSTOMER_UUID", ""),
     "tenant_uuid" => env("BULLWARK_TENANT_UUID", ""),
     "dev_mode" => env("BULLWARK_DEV_MODE", in_array(env("environment"), ["development", "dev", "local"])),
