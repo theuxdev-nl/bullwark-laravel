a59aaa91e2baee122a620c4c39ccfc7293e0723d
Paul Brouwer <pbrouwer@offertevergelijker.nl>
2025-12-07 01:16:23 +0100

    package update

--- src/Config/bullwark.php
+++ src/Config/bullwark.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'api_url' => env("BULLWARK_API_URL", "http://paulpc-1/api/v1"),
-    'jwk_url' => env("BULLWARK_JWK_URL", "http://paulpc-1/.well-known/jwks.json"),
+    'api_url' => env("BULLWARK_API_URL", ""),
+    'jwk_url' => env("BULLWARK_JWK_URL", ""),
     "customer_uuid" => env("BULLWARK_CUSTOMER_UUID", ""),
     "tenant_uuid" => env("BULLWARK_TENANT_UUID", ""),
     "dev_mode" => env("BULLWARK_DEV_MODE", in_array(env("environment"), ["development", "dev", "local"])),
